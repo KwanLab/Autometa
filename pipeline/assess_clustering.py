375b2da9aa1aff2360fc0d420984007ca5201a56
Jason Kwan <jason.kwan@wisc.edu>
2016-05-26 09:09:54 -0500

    changed algorithm in assess_clustering.py

--- pipeline/assess_clustering.py
+++ pipeline/assess_clustering.py
@@ -9,12 +9,14 @@ parser.add_argument('-s','--hmmtable', help='HMM table created by make_marker_ta
 parser.add_argument('-d', '--dbscantable', help='Table(s) containing DBSCAN clusters', nargs='+', required=True)
 parser.add_argument('-o', '--output', help='Output table path', required=True)
 parser.add_argument('-c', '--column', help='bin column name', default = 'db.cluster')
+parser.add_argument('-k', '--kingdom', help='Kingdom under consideration (bacteria|archaea)', default = 'bacteria')
 args = vars(parser.parse_args())
 
 hmm_contig_table_path = args['hmmtable']
 dbscan_table_paths = args['dbscantable']
 output_table_path = args['output']
 dbscan_column_heading = args['column']
+kingdom = args['kingdom']
 
 # First parse hmm_contig_table_path
 
@@ -36,8 +38,10 @@ for i,line in enumerate(hmm_contig_table_rows):
 				contig_markers[contig][pfam] = 1
 
 # Now we go through the dbscan tables
-table_completeness_averages = {}
-table_contamination_averages = {}
+#table_completeness_averages = {}
+#table_contamination_averages = {}
+table_binned_unique_marker_counter = {} # Keeps a total of unique markers in each bin, as long as the bin contains more than 20% of the total
+# In Bacteria, the total is 139, in Archaea, the total is 162
 
 for dbscan_table_path in dbscan_table_paths:
 	print ('Considering ' + dbscan_table_path)
@@ -91,31 +95,44 @@ for dbscan_table_path in dbscan_table_paths:
 						bin_markers[cluster][pfam] = contig_markers[contig][pfam]
 
 	# Now work out the number duplicated for each cluster
-	duplicated = [] # Just a list of counts for duplicated markers in each cluster
+	#duplicated = [] # Just a list of counts for duplicated markers in each cluster
+	table_unique_markers_counter = 0
 	for cluster in bin_markers:
-		duplicated_counter = 0
+		not_duplicated_counter = 0
 		for pfam in bin_markers[cluster]:
-			if bin_markers[cluster][pfam] > 1:
-				duplicated_counter += 1
-		duplicated.append(duplicated_counter)
+			if bin_markers[cluster][pfam] == 1:
+				not_duplicated_counter += 1
+		#duplicated.append(duplicated_counter)
+		threshold = None
+		if kingdom == 'bacteria':
+			threshold = 28
+		elif kingdom == 'archaea':
+			threshold = 32
+		if not_duplicated_counter > threshold:
+			table_unique_markers_counter += not_duplicated_counter
 
-	average_duplicated = sum(duplicated) / len(duplicated)
-	table_contamination_averages[dbscan_table_path] = average_duplicated
+	table_binned_unique_marker_counter[dbscan_table_path] = table_unique_markers_counter
+
+	#average_duplicated = sum(duplicated) / len(duplicated)
+	#table_contamination_averages[dbscan_table_path] = average_duplicated
 
 	# Now work out the number of markers each cluster has
-	number_of_markers = [] # Just a list of counts of unique markers in each cluster
-	for cluster in bin_markers:
-		number_of_markers.append(len(bin_markers[cluster]))
+	#number_of_markers = [] # Just a list of counts of unique markers in each cluster
+	#for cluster in bin_markers:
+	#	number_of_markers.append(len(bin_markers[cluster]))
 
-	average_markers = sum(number_of_markers) / len(number_of_markers)
-	table_completeness_averages[dbscan_table_path] = average_markers
+	#average_markers = sum(number_of_markers) / len(number_of_markers)
+	#table_completeness_averages[dbscan_table_path] = average_markers
 
 
 # Print output table
 output_table = open(output_table_path, 'w')
-output_table.write('table\tav_number_of_markers\tav_duplicated_markers\n')
+#output_table.write('table\tav_number_of_markers\tav_duplicated_markers\n')
+output_table.write('table\tnumber_binned_unique_markers\n')
 
 for table_path in table_completeness_averages:
-	completeness = table_completeness_averages[table_path]
-	duplicated = table_contamination_averages[table_path]
-	output_table.write(table_path + '\t' + str(completeness) + '\t' + str(duplicated) + '\n')
+	#completeness = table_completeness_averages[table_path]
+	#duplicated = table_contamination_averages[table_path]
+	#output_table.write(table_path + '\t' + str(completeness) + '\t' + str(duplicated) + '\n')
+	number_unique_markers = table_binned_unique_marker_counter[table_path]
+	output_table.write(table_path + '\t' + str(number_unique_markers) + '\n')
