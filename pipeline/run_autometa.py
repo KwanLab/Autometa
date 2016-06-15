6aba2881ee3b1d4a2ae5d78d6fb629b437ee0eeb
Ian Miller <ijmiller2@wisc.edu>
2016-06-15 17:26:58 -0500

    Fixed call to function

--- pipeline/run_autometa.py
+++ pipeline/run_autometa.py
@@ -44,7 +44,7 @@ def make_marker_table(fasta):
 	output_marker_table = fasta.split('.')[0] + "_marker.tab"
 	if os.path.isfile(output_marker_table):
 		print "{} file already exists!".format(output_marker_table)
-		return
+		print "Continuing to next step..."
 	subprocess.call("hmmpress -f {}".format(hmm_marker_path), shell=True)
 	subprocess.call("{}make_marker_table.py -a {} -m {} -c {} -o {} -p {}".format(pipeline_path,fasta, hmm_marker_path, hmm_cutoffs_path,output_marker_table,args['processors']), shell = True)
 	return output_marker_table
