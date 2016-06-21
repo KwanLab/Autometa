68886a02aecca42a5132dd0255117f8edde748c5
Ian Miller <ijmiller2@wisc.edu>
2016-06-21 10:50:58 -0500

    Fixed dbscan issued (was a typo)

--- pipeline/dbscan_batch.R
+++ pipeline/dbscan_batch.R
@@ -1,8 +1,8 @@
 # Program to run dbscan over a range of eps values
 # USAGE: Rscript dbscan_batch.r <input contig table> <start eps> <end eps> [<eps step> (default = 0.1)]
 
-library(dbscan)
-library(docopt)
+library('dbscan')
+library('docopt')
 
 # args is a named list
 args <- docopt('USAGE: dbscan_batch.R <input_contig_table> <start_eps> <end_eps> [eps_step]')
@@ -23,7 +23,7 @@ for (i in eps_values) {
 	# First determine what the output file will be called
 	output_filename = paste(args[["input_contig_table"]], "eps", toString(i), sep="_")
 	d <- data.frame(data$vizbin_x, data$vizbin_y)
-	db <- dbscan(data, eps=i, minPts=3)
+	db <- dbscan(d, eps=i, minPts=3)
 	output_table <- data.frame(data, db.cluster=db$cluster)
 	write.table(output_table, file=output_filename, sep="\t", quote=FALSE, row.names=FALSE)
 }
