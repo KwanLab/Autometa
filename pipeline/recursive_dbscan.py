29fe8e1a12d72a2a76bd21e08928f68b8c0584c9
Jason Kwan <jason.kwan@wisc.edu>
2016-07-16 08:28:41 -0500

    updated recursive_dbscan.py

--- pipeline/recursive_dbscan.py
+++ pipeline/recursive_dbscan.py
@@ -305,6 +305,7 @@ def run_VizBin(fasta, output_filename):
 	else:
 		print "Runnign k-mer based binning..."
 		logger.info('Running k-mer based binning...')
+		print("java -jar {}VizBin-dist.jar -i {} -o points.txt".format(autometa_path + "/VizBin/dist/", fasta))
 		subprocess.call("java -jar {}VizBin-dist.jar -i {} -o points.txt".format(autometa_path + "/VizBin/dist/", fasta), shell = True, stdout=FNULL, stderr=subprocess.STDOUT)
 		tmp_path = subprocess.check_output("ls /tmp/map* -dlt | grep {} | head -n1".format(username), shell=True).rstrip("\n").split()[-1]
 		return tmp_path
