1b2084ae3ea31715534effc68a0c48f1e00d8fa7
Jason Kwan <jason.kwan@wisc.edu>
2017-05-18 09:25:28 -0500

    reverted assessDBSCAN logic and kept minPts=1 in recursive_dbscan.py

--- pipeline/recursive_dbscan.py
+++ pipeline/recursive_dbscan.py
@@ -233,9 +233,9 @@ def assessDBSCAN(table_dictionary, hmm_dictionary, domain, completeness_cutoff, 
 		completeness_product[eps] = number_complete_and_pure_clusters[eps] * mean_completeness[eps]
 
 	# Get eps value with highest number of complete clusters
-	sorted_eps_values = sorted(number_complete_and_pure_clusters, key=number_complete_and_pure_clusters.__getitem__, reverse=True)
+	#sorted_eps_values = sorted(number_complete_and_pure_clusters, key=number_complete_and_pure_clusters.__getitem__, reverse=True)
 	#sorted_eps_values = sorted(median_completeness, key=median_completeness.__getitem__, reverse=True)
-	#sorted_eps_values = sorted(mean_completeness, key=mean_completeness.__getitem__, reverse=True)
+	sorted_eps_values = sorted(mean_completeness, key=mean_completeness.__getitem__, reverse=True)
 	best_eps_value = sorted_eps_values[0]
 
 	#pdb.set_trace()
