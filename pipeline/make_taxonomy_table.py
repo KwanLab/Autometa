e61871f02958a925e97b94980787233310da1702
Ian Miller <ijmiller2@wisc.edu>
2017-07-01 13:11:44 -0500

    Fixed some path hadnling bugs and added a check for empty .daa files

--- pipeline/make_taxonomy_table.py
+++ pipeline/make_taxonomy_table.py
@@ -17,13 +17,13 @@ args = vars(parser.parse_args())
 
 num_processors = args['processors']
 length_cutoff = args['length_cutoff']
-fasta_assembly = os.path.basename(args['assembly'])
+fasta_path = args['assembly']
+fasta_assembly_prefix = os.path.splitext(os.path.basename(args['assembly']))[0]
 
-
-def length_trim(fasta,length_cutoff):
+def length_trim(fasta_path,fasta_prefix,length_cutoff):
 	#Trim the length of fasta file
-	outfile_name = str(args['assembly'].split("/")[-1].split(".")[0]) + "_filtered.fasta"
-	subprocess.call("{}/fasta_length_trim.pl {} {} {}".format(pipeline_path, fasta, length_cutoff, outfile_name), shell = True)
+	outfile_name = str(fasta_prefix) + "_filtered.fasta"
+	subprocess.call("{}/fasta_length_trim.pl {} {} {}".format(pipeline_path, fasta_path, length_cutoff, outfile_name), shell = True)
 	return outfile_name
 
 def run_prodigal(path_to_assembly):
@@ -37,24 +37,12 @@ def run_prodigal(path_to_assembly):
 	 	'.orfs.faa','-p meta', '-m', '-o ' + path_to_assembly.split(".")[0] + '.txt']), shell = True)
 
 def run_diamond(prodigal_output, diamond_database_path, num_processors, prodigal_daa):
-	view_output = prodigal_output + ".tab"
-	current_dir = os.getcwd()
-	tmp_dir_path = current_dir + '/tmp'
-	if not os.path.isdir(tmp_dir_path):
-		os.makedirs(tmp_dir_path) # This will give an error if the path exists but is a file instead of a dir
-	if os.path.isfile(prodigal_daa):
-		print "{} file already exists!".format(prodigal_daa)
-		print 'Continuing to next step...'
-	else:
-		subprocess.call("diamond blastp --query {}.faa --db {} --evalue 1e-5 --max-target-seqs 200 -p {} --daa {} -t {}"\
-			.format(prodigal_output, diamond_database_path, num_processors, prodigal_daa, tmp_dir_path), shell = True)
+    view_output = prodigal_output + ".tab"
+    current_dir = os.getcwd()
+    subprocess.call("diamond blastp --query {}.faa --db {} --evalue 1e-5 --max-target-seqs 200 -p {} --daa {}".format(prodigal_output, diamond_database_path, num_processors, prodigal_daa), shell = True)
+    subprocess.call("diamond view -a {} -f tab -o {}".format(prodigal_daa, view_output), shell = True)
+    return view_output
 
-	if os.path.isfile(view_output):
-		print "{} file already exists!".format(view_output)
-		print "Continuing to next setp..."
-	else:
-		subprocess.call("diamond view -a {} -f tab -o {}".format(prodigal_daa, view_output), shell = True)
-	return view_output
 	#return  view_output
 	#might want to change name of outputfile
 
@@ -71,13 +59,13 @@ def run_blast2lca(input_file):
 def run_taxonomy(pipeline_path, assembly_path, tax_table_path, taxdump_dir_path): #Have to update this
 	initial_table_path = assembly_path + '.tab'
 	subprocess.call("{}/make_contig_table.py {} {}".format(pipeline_path, assembly_path, initial_table_path), shell = True)
-	subprocess.call("{}/add_contig_taxonomy.py {} {} {} taxonomy.tab".format(pipeline_path, initial_table_path ,tax_table_path, taxdump_dir_path), shell = True)
+	subprocess.call("{}/add_contig_taxonomy.py {} {} {} taxonomy.tab".format(pipeline_path, initial_table_path,tax_table_path, taxdump_dir_path), shell = True)
 	#contig_table_path, tax_table_path, taxdump_dir_path, output_file_path
 	return 'taxonomy.tab'
 
 #diamond_path = subprocess.check_output('find ~ -name "diamond"', shell=True).rstrip("\n") # assume diamond is in the path
 taxdump_dir_path = '/home/jkwan/blast2lca_taxdb'
-prodigal_output = args['assembly'].split('.')[0] + "_filtered.orfs"
+prodigal_output = fasta_assembly_prefix + "_filtered.orfs"
 prodigal_daa = prodigal_output + ".daa"
 pipeline_path = sys.path[0]
 pathList = pipeline_path.split('/')
@@ -86,23 +74,29 @@ autometa_path = '/'.join(pathList)
 #diamond_database_path = subprocess.check_output('find /mnt/not_backed_up/nr_diamond/ -name "nr.dmnd"', shell=True).strip("\n")
 diamond_database_path = '/media/box2/nr_old/nr'
 #add_contig_path = pipeline_path
-filtered_assembly = str(args['assembly'].split("/")[-1].split(".")[0]) + "_filtered.fasta"
+filtered_assembly = fasta_assembly_prefix + "_filtered.fasta"
 
 if not os.path.isfile(prodigal_output + ".faa"):
 	print "Prodigal output not found. Running prodigal..."
 	#Check for file and if it doesn't exist run make_marker_table
-	length_trim(fasta_assembly, length_cutoff)
+	length_trim(fasta_path, fasta_assembly_prefix, length_cutoff)
 	run_prodigal(filtered_assembly)
 
-
-if not os.path.isfile(prodigal_output + ".tab"):
-	print "Running diamond blast... "
+if not os.path.isfile(prodigal_output + ".daa"):
+	print "Could not find {}. Running diamond blast... ".format(prodigal_output + ".daa")
 	#diamond_output =
+	diamond_output = run_diamond(prodigal_output, diamond_database_path, num_processors, prodigal_daa)
+elif os.stat(prodigal_output + ".daa").st_size == 0:
+	print "{} files empty. Re-running diamond blast...".format(prodigal_output + ".tab")
 	diamond_output = run_diamond(prodigal_output, diamond_database_path, num_processors, prodigal_daa)
 else:
 	diamond_output = prodigal_output + ".tab"
 
-blast2lca_output = run_blast2lca(diamond_output)
+if not os.path.isfile(prodigal_output + ".lca"):
+    blast2lca_output = run_blast2lca(diamond_output)
+else:
+    blast2lca_output = prodigal_output + ".lca"
+
 print "Running add_contig_taxonomy.py... "
 taxonomy_table = run_taxonomy(pipeline_path, filtered_assembly, blast2lca_output, taxdump_dir_path)
 
