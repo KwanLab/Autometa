916f149b5ff50b423f3c141ff89a7af31156b084
Jason Kwan <jason.kwan@wisc.edu>
2017-08-25 13:21:03 -0500

    Changed run_autometa.py so that output dir can be specified, and also changed recursive_dbscan.py algorithm to simple recursive DBSCAN (2+3 dimensions), with optional taxonomy splitting

--- pipeline/run_autometa.py
+++ pipeline/run_autometa.py
@@ -12,17 +12,19 @@ import logging
 
 def length_trim(fasta,length_cutoff):
 	#will need to update path of this perl script
-	outfile_name = str(args['a'].split("/")[-1].split(".")[0]) + "_filtered.fasta"
-	logger.info("{}/fasta_length_trim.pl {} {} {}".format(pipeline_path, fasta, length_cutoff,outfile_name))
-	subprocess.call("{}/fasta_length_trim.pl {} {} {}".format(pipeline_path, fasta, length_cutoff,outfile_name), shell = True)
+	outfile_name = os.path.basename(fasta).split(".")[0]) + "_filtered.fasta"
+	output_path = output_dir + '/' + outfile_name
+	logger.info("{}/fasta_length_trim.pl {} {} {}".format(pipeline_path, fasta, length_cutoff,output_path))
+	subprocess.call("{}/fasta_length_trim.pl {} {} {}".format(pipeline_path, fasta, length_cutoff,output_path), shell = True)
 	return outfile_name
 
 def make_contig_table(fasta):
 	#looks like this script is assuming contigs from a spades assembly
 	output_table_name = str(fasta).split('.')[0] + ".tab"
-	logger.info("{}/make_contig_table.py {} {}".format(pipeline_path,fasta,output_table_name))
-	subprocess.call("{}/make_contig_table.py {} {}".format(pipeline_path,fasta,output_table_name), shell = True)
-	return output_table_name
+	output_path = output_dir + '/' + output_table_name
+	logger.info("{}/make_contig_table.py {} {}".format(pipeline_path,fasta,output_path))
+	subprocess.call("{}/make_contig_table.py {} {}".format(pipeline_path,fasta,output_path, shell = True)
+	return output_path
 
 def make_marker_table(fasta):
 	if kingdom == 'bacteria':
@@ -34,7 +36,8 @@ def make_marker_table(fasta):
 
 	#need to add processors to this script
 	output_marker_table = fasta.split('.')[0] + "_marker.tab"
-	if os.path.isfile(output_marker_table):
+	output_path = output_dir + '/' + output_marker_table
+	if os.path.isfile(output_path):
 		print "{} file already exists!".format(output_marker_table)
 		print "Continuing to next step..."
 		logger.info('{} file already exists!'.format(output_marker_table))
@@ -45,19 +48,37 @@ def make_marker_table(fasta):
 		logger.info("hmmpress -f {}".format(hmm_marker_path))
 		subprocess.call("hmmpress -f {}".format(hmm_marker_path), shell=True,stdout=FNULL, stderr=subprocess.STDOUT)
 		logger.info("{}/make_marker_table.py -a {} -m {} -c {} -o {} -p {}".\
-			format(pipeline_path,fasta, hmm_marker_path, hmm_cutoffs_path,output_marker_table, args['p']))
+			format(pipeline_path,fasta, hmm_marker_path, hmm_cutoffs_path,output_path, args['p']))
 		subprocess.call("{}/make_marker_table.py -a {} -m {} -c {} -o {} -p {}".\
-			format(pipeline_path,fasta, hmm_marker_path, hmm_cutoffs_path,output_marker_table, args['p']), \
+			format(pipeline_path,fasta, hmm_marker_path, hmm_cutoffs_path,output_path, args['p']), \
 			shell = True, stdout=FNULL, stderr=subprocess.STDOUT)
-	return output_marker_table
+	return output_path
 
-def bin_assess_and_pick_cluster(pipeline_path,marker_tab, filtered_assembly, contig_table):
-	if taxonomy_table_path:
-		logger.info("{}/recursive_dbscan.py -m {} -d {} -f {} -o ./ -c {} -p {} -t {}".format(pipeline_path,marker_tab,kingdom,filtered_assembly, contig_table, processors, taxonomy_table_path))
-		subprocess.call("{}/recursive_dbscan.py -m {} -d {} -f {} -o ./ -c {} -p {} -t {}".format(pipeline_path,marker_tab,kingdom,filtered_assembly, contig_table, processors, taxonomy_table_path), shell=True)
-	else:
-		logger.info("{}/recursive_dbscan.py -m {} -d {} -f {} -o ./ -c {} -p {}".format(pipeline_path,marker_tab,kingdom,filtered_assembly, contig_table, processors))
-		subprocess.call("{}/recursive_dbscan.py -m {} -d {} -f {} -o ./ -c {} -p {}".format(pipeline_path,marker_tab,kingdom,filtered_assembly, contig_table, processors), shell=True)
+def recursive_dbscan(input_table, filtered_assembly, domain):
+	recursive_dbscan_output_path = output_dir + '/recursive_dbscan_output.tab'
+
+	logger.info("{}/recursive_dbscan.py -t {} -a {} -o {} -k {}".format(pipeline_path, input_table, filtered_assembly, domain))
+	subprocess.call("{}/recursive_dbscan.py -t {} -a {} -o {} -k {}".format(pipeline_path, input_table, filtered_assembly, domain), shell=True)
+
+	return recursive_dbscan_output_path
+
+def combine_tables(table1_path, table2_path):
+	table1_name = os.path.basename(table1_path)
+	table2_name = os.path.basename(table2_path)
+	comb_table_path = output_dir + '/combined_contig_info.tab'
+	script = 'head -n 1 ' + table1_path + ' > ' + output_dir + '/' + table1_name + '_header\n'\
+		+ 'tail -n +2 ' + table1_path + ' | sort -k 1,1 > ' + output_dir + '/' + table1_name + '_sort\n'\
+		+ 'head -n 1 ' + table2_path + ' > ' + output_dir + '/' + table2_name + '_header\n'\
+		+ 'tail -n +2 ' + table2_path + ' > ' + output_dir + '/' + table2_name + '_sort\n'\
+		+ "{join -t $'\\t' " + output_dir + '/' + table1_name + '_header ' + output_dir + '/' + table2_name + "_header; join -t $'\\t' " + output_dir + '/' + table1_name + '_sort ' + output_dir + '/' + table2_name + '_sort} > ' + comb_table_path
+	os.system("bash -c '%s'" % script)
+	cleanup_script = 'rm ' + output_dir + '/' + table1_name + '_header\n'\
+		+ 'rm ' + output_dir + '/' + table1_name + '_sort\n'\
+		+ 'rm ' + output_dir + '/' + table2_name + '_header\n'\
+		+ 'rm ' + output_dir + '/' + table2_name + '_sort'
+	os.system("bash -c '%s'" % cleanup_script)
+
+	return comb_table_path
 
 #logger
 logger = logging.getLogger('run_autometa.py')
@@ -79,14 +100,16 @@ parser.add_argument('-c', metavar='cluster completeness output', help='Best clus
 parser.add_argument('-k', metavar='kingdom', help='Kingdom to consider (archaea|bacteria)',\
 choices=['bacteria','archaea'], default = 'bacteria')
 parser.add_argument('-t', metavar='taxonomy table', help='Output of make_taxonomy_table.py')
+parser.add_argument('-o', metavar='output directory', help='Directory to store all output files', default = '.')
 args = vars(parser.parse_args())
 
 length_cutoff = args['l']
-fasta_assembly = os.path.basename(args['a'])
+fasta_assembly = os.path.abspath(args['a'])
 processors = args['p']
 cluster_completeness = args['c']
 kingdom = args['k'].lower()
 taxonomy_table_path = args['t']
+output_dir = args['o']
 
 #check if fasta in path
 if not os.path.isfile(args['a']):
@@ -95,7 +118,10 @@ if not os.path.isfile(args['a']):
 	exit()
 
 #what input variables were and when you ran it (report fill path based on argparse)
-logger.info('Input: -a {} -p {} -l {} -c {}'.format(args['a'], processors, length_cutoff, cluster_completeness))
+if taxonomy_table_path:
+	logger.info('Input: -a {} -p {} -l {} -c {} -k {} -t {} -o {}'.format(args['a'], processors, length_cutoff, cluster_completeness, kingdom, taxonomy_table_path, output_dir))
+else:
+	logger.info('Input: -a {} -p {} -l {} -c {} -k {} -o {}'.format(args['a'], processors, length_cutoff, cluster_completeness, kingdom, output_dir))
 
 start_time = time.time()
 FNULL = open(os.devnull, 'w')
@@ -118,12 +144,17 @@ commit = subprocess.Popen(commit_command, shell=True, stdout=subprocess.PIPE).co
 logger.info('Currently running branch ' + branch + ', commit ' + commit)
 
 #run length trim and store output name
-filtered_assembly = length_trim(args['a'], args['l'])
+filtered_assembly = length_trim(fasta_assembly, length_cutoff)
 contig_table = make_contig_table(filtered_assembly)
 marker_tab_path = make_marker_table(filtered_assembly)
-vizbin_output_path = "contig_vizbin.tab"
 
-bin_assess_and_pick_cluster(pipeline_path, marker_tab_path, filtered_assembly, contig_table)
+# Make combined table
+if taxonomy_table_path:
+	combined_table_path = combine_tables(taxonomy_table_path, marker_tab_path)
+else:
+	combined_table_path = combine_tables(contig_table, marker_tab_path)
+
+recursive_dbscan_output = recursive_dbscan(combined_table_path, filtered_assembly, kingdom)
 
 elapsed_time = time.strftime('%H:%M:%S', time.gmtime(round((time.time() - start_time),2)))
 
