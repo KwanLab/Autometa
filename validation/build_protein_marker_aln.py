e1db73f57b554d91c15a6441f6364816f39f7e11
Ian Miller <ijmiller2@wisc.edu>
2017-12-07 14:14:15 -0600

    I've got it all working now. Checks for duplicate alignments, alignment lengths, and masked residues. Output concatenated .aln file

--- validation/build_protein_marker_aln.py
+++ validation/build_protein_marker_aln.py
@@ -3,10 +3,13 @@ import glob
 import os
 import re
 import subprocess
+import pandas as pd
+from collections import Counter
 
 #input = whole genome fastas
 #output = aligned fasta file with all shared protein markers
 """
+0. Standardize fasta contig names
 1. Call ORFs, translate, and identify markers with AMPHORA’s MarkerScanner.pl
 2. Combine protein marker sequences with cat
 3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScanner.pl
@@ -19,9 +22,31 @@ import subprocess
 #NOTE: Had to manually add "AAA.1"  toB. luti and B. thetaiotaomicron seq names
 #to have MarkerScanner.pl parse their names properly
 
+ref_genome_table = pd.read_csv("ncbi_ref_genomes.tab",sep="\t")
+genome_filename_dict = {}
+
+#0. Standardize fasta contig names
+for index,row in ref_genome_table.iterrows():
+    basename = row['basename']
+    genome_name = row['ref_genome']
+    genome_filename_dict[basename] = genome_name
+
+for fna in glob.glob("*.fna"):
+    basename = ".".join(fna.split(".")[:-1])
+    output_handle = basename + ".fasta"
+    input_handle = open(fna)
+    with open(output_handle,"w") as outfile:
+        for count,line in enumerate(input_handle):
+            if ">" in line:
+                seq_name = line.rstrip().lstrip(">")
+                outfile.write(">" + basename + "-" + str(count) + "\n")
+            else:
+                outfile.write(line)
+    input_handle.close()
+
 #1. For every fasta in the working directory
 num_input_genome = 0
-for fasta in glob.glob("*.fna"):
+for fasta in glob.glob("*.fasta"):
     #Make a new directory with the fastas base name, if it doesn't exist
     basename = ".".join(fasta.split(".")[:-1])
     if not os.path.isdir(basename):
@@ -34,7 +59,7 @@ for fasta in glob.glob("*.fna"):
 
 #2. Combine protein marker sequences with cat
 #subprocess.call("find . -mindepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True)
-subprocess.call("find . -mindepth 2 -maxdepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True) #To omit B. luti seqs
+subprocess.call("find . -mindepth 2 -maxdepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True)
 
 #3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScanner.pl
 subprocess.call(["mkdir","combined"])
@@ -43,17 +68,6 @@ subprocess.call(["MarkerScanner.pl","-Bacteria","../combined.pep"])
 
 #4. Align markers with AMPHORA’s MarkerAlignTrim.pl
 subprocess.call(["MarkerAlignTrim.pl","-OutputFormat","fasta"])
-
-#Testing in: /home/ijmiller/AMPHORA2/MIX-51/combined
-#dnaG.aln has 3 sequences and rplT.aln has 1
-
-#4a. Identify list of shared sequences
-#wd: /home/ijmiller/AMPHORA2/MIX-51/GCA_000156495.1_ASM15649v1_genomic perl ../Scripts/MarkerScanner.pl -DNA \-Bacteria /home/ijmiller/MIX-51/reference_fasta_files/GCA_000156495.1_ASM15649v1_genomic.fna(Do the same in /home/ijmiller/AMPHORA2/MIX-51//GCA_000012825.1_ASM1282v1_genomic2.
-#Combine protein marker sequences with cat
-#wd: /home/ijmiller/AMPHORA2/MIX-51/combined
-#cat ../GCA_000156495.1_ASM15649v1_genomic/*pep \../GCA_000012825.1_ASM1282v1_genomic/*pep>combined.pep3.
-#Reformat combined protein sequence file with another run of AMPHORA’s
-#perl ../../Scripts/MarkerScanner.pl -Bacteria combined.pepThis effectivelly separates out all of the markers into individual multifasta .pep files that appear consistent informat with media/box1/AMPHORA_example/AMPHORA/cobmined/*.pep1
 aln_list = glob.glob("*.aln")
 if os.path.exists("combined.aln"):
     aln_list.remove("combined.aln")
@@ -65,19 +79,25 @@ shared_protein_list = []
 marker_list = []
 genome_dict = {}
 num_marker_proteins = 0
+skip_sequence = False
 for aln in aln_list:
     num_marker_proteins += 1
     protein_name = aln.rstrip(".aln")
     marker_list.append(protein_name)
     with open(aln) as infile:
         num_aln_genomes = 0
+        aln_dict = {}
         for line in infile:
+
             if ">" in line:
+                skip_sequence = False
                 seq_name = line.rstrip().lstrip(">")
                 genome = seq_name.split("SENSE) ")[-1]
-                m = re.search(r"[A-Z]{1}[a-z]+\s[a-z]+", seq_name)
+                #m = re.search(r"[A-Z]{1}[a-z]+\s[a-z]+", seq_name)
+                m = re.search(r">[A-Z]+.*?-[0-9]",line.rstrip())
                 if m:
-                    genome = m.group()
+                    genome_basename = m.group()[1:-2].rstrip("_genomic")
+                    genome = genome_filename_dict[genome_basename]
                 else:
                     print("No match found:\n{}\nExitting...".format(seq_name))
                     print("Make sure to delete files before rerunning AMPHORA scripts...")
@@ -88,24 +108,30 @@ for aln in aln_list:
                     genome_dict[genome] = {}
                     genome_dict[genome][protein_name] = ""
                     num_aln_genomes += 1
+                    aln_dict[protein_name]=[genome]
                 elif protein_name not in genome_dict[genome]:
                     genome_dict[genome][protein_name] = ""
                     num_aln_genomes += 1
+                    #aln_dict[protein_name].append(genome)
                 else:
-                    print("Duplicate protein for {}\nOmitting duplicate...".format(protein_name))
+                    skip_sequence = True
+                    print("Ommitting duplicate {} protein for {}...".format(protein_name,genome))
 
+            elif skip_sequence: #Skip duplicate markers
+                continue
             else:
                 genome_dict[genome][protein_name] += line.rstrip()
+
         if num_aln_genomes == num_input_genome:
+        #if len(aln_dict.keys()) == num_input_genome:
             #This will not properly handle the case of multiple protein
             #seqs from single genome
             shared_protein_list.append(protein_name)
 
-
         else:
-            print("{} genomes have {}".format(num_aln_genomes,protein_name))
+            #print("{} genomes have {}".format(num_aln_genomes,protein_name))
             print("{} not presented in all {} genomes, omitting from tree...".format(protein_name,num_input_genome))
-print("\nUsing {} of {} protein markers to build concatenated alignment...".format(num_marker_proteins,len(shared_protein_list)))
+print("\nUsing {} of {} protein markers to build concatenated alignment...".format(len(shared_protein_list),num_marker_proteins))
 
 if len(shared_protein_list) == 0:
     for genome in genome_dict.keys():
@@ -114,8 +140,18 @@ if len(shared_protein_list) == 0:
         if len(missing_markers) > 0:
             print ("\tMissing: {}".format(",".join(missing_markers)))
 
-#Blautia luti is missing: rpsJ,rplT,tsf,rpmA,rplF,rplM
-#Could omit this one...
+#NOTE: Should check length - Jason's note in 'amphora_merge_alignments.pl'
+# Sometimes APMPHORA2 misses a character on the end of the last sequence
+# Check the alignment lengths
+aln_length_dict = {}
+for shared_protein in shared_protein_list:
+    for genome in genome_dict.keys():
+        protein_aln_len = len(genome_dict[genome][shared_protein])
+        if not shared_protein in aln_length_dict:
+            aln_length_dict[shared_protein] = [protein_aln_len]
+        else:
+            aln_length_dict[shared_protein].append(protein_aln_len)
+    print shared_protein,Counter(aln_length_dict[shared_protein])
 
 #6. Correct and concatenate aligned marker sequences
 combined_aln_dict = {}
@@ -124,9 +160,6 @@ for genome in genome_dict.keys():
     for count,shared_protein in enumerate(shared_protein_list):
         print genome,shared_protein
         seq = genome_dict[genome][shared_protein]
-        #NOTE: Should check length - Jason's note in 'amphora_merge_alignments.pl'
-        # Sometimes APMPHORA2 misses a character on the end of the last sequence
-    	# Check the alignment lengths
         corrected_seq = ""
         #Replace masked letters
         for letter in seq:
