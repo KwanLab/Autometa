e250733a313fb07a53bcc48ee8e5671c10d7b579
Ian Miller <ijmiller2@wisc.edu>
2017-12-07 11:04:55 -0600

    Got marker detection, alignment, and concatenation working based on a directory of input fastas. Still working to filter a subset of sequences and genomes that contain shared sequences.

--- validation/build_protein_marker_aln.py
+++ validation/build_protein_marker_aln.py
@@ -1,38 +1,74 @@
 
+import glob
+import os
+import re
+import subprocess
 
 #input = whole genome fastas
 #output = aligned fasta file with all shared protein markers
-
+"""
 1. Call ORFs, translate, and identify markers with AMPHORA’s MarkerScanner.pl
 2. Combine protein marker sequences with cat
 3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScanner.pl
 •This creates .pep files that appear consistent in format with/media/box1/AMPHORA_example/AMPHORA/cobmined/*.pep
 4. Align markers with AMPHORA’s MarkerAlignTrim.pl
-5. Identify list of shared markers with Python script
-6. Concatenate aligned marker sequences with amphora_alignment_filter_and_concat.pl
-7. Build tree from alignment with FastTreeMP1. Call ORFs, translate, and identify markers with AMPHORA’s MarkerScanner.pl#wd: /home/ijmiller/AMPHORA2/MIX-51/GCA_000156495.1_ASM15649v1_genomicperl ../Scripts/MarkerScanner.pl -DNA \-Bacteria /home/ijmiller/MIX-51/reference_fasta_files/GCA_000156495.1_ASM15649v1_genomic.fna(Do the same in /home/ijmiller/AMPHORA2/MIX-51//GCA_000012825.1_ASM1282v1_genomic2. Combine protein marker sequences with cat#wd: /home/ijmiller/AMPHORA2/MIX-51/combinedcat ../GCA_000156495.1_ASM15649v1_genomic/*pep \../GCA_000012825.1_ASM1282v1_genomic/*pep>combined.pep3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScan-ner.plperl ../../Scripts/MarkerScanner.pl -Bacteria combined.pepThis effectivelly separates out all of the markers into individual multifasta .pep files that appear consistent informat with media/box1/AMPHORA_example/AMPHORA/cobmined/*.pep1
-#1. Run
+5. Identify list of shared markers and extract individual aligned sequences
+6. Correct and concatenate aligned marker sequences
+7. Build tree from alignment with FastTreeMP.
+"""
+#NOTE: Had to manually add "AAA.1"  toB. luti and B. thetaiotaomicron seq names
+#to have MarkerScanner.pl parse their names properly
 
+#1. For every fasta in the working directory
+num_input_genome = 0
+for fasta in glob.glob("*.fna"):
+    #Make a new directory with the fastas base name, if it doesn't exist
+    basename = ".".join(fasta.split(".")[:-1])
+    if not os.path.isdir(basename):
+        subprocess.call(["mkdir",basename])
+    #Run MarkerScanner.pl in the new folder
+    os.chdir(basename)
+    subprocess.call(["MarkerScanner.pl", "-DNA","-Bacteria","../"+fasta])
+    os.chdir("../")
+    num_input_genome += 1
 
-#4. Concatenate .aln files
+#2. Combine protein marker sequences with cat
+#subprocess.call("find . -mindepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True)
+subprocess.call("find . -mindepth 2 -maxdepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True) #To omit B. luti seqs
+
+#3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScanner.pl
+subprocess.call(["mkdir","combined"])
+os.chdir("combined")
+subprocess.call(["MarkerScanner.pl","-Bacteria","../combined.pep"])
+
+#4. Align markers with AMPHORA’s MarkerAlignTrim.pl
+subprocess.call(["MarkerAlignTrim.pl","-OutputFormat","fasta"])
 
 #Testing in: /home/ijmiller/AMPHORA2/MIX-51/combined
 #dnaG.aln has 3 sequences and rplT.aln has 1
 
-#4a. Identify lift of shared sequences
+#4a. Identify list of shared sequences
+#wd: /home/ijmiller/AMPHORA2/MIX-51/GCA_000156495.1_ASM15649v1_genomic perl ../Scripts/MarkerScanner.pl -DNA \-Bacteria /home/ijmiller/MIX-51/reference_fasta_files/GCA_000156495.1_ASM15649v1_genomic.fna(Do the same in /home/ijmiller/AMPHORA2/MIX-51//GCA_000012825.1_ASM1282v1_genomic2.
+#Combine protein marker sequences with cat
+#wd: /home/ijmiller/AMPHORA2/MIX-51/combined
+#cat ../GCA_000156495.1_ASM15649v1_genomic/*pep \../GCA_000012825.1_ASM1282v1_genomic/*pep>combined.pep3.
+#Reformat combined protein sequence file with another run of AMPHORA’s
+#perl ../../Scripts/MarkerScanner.pl -Bacteria combined.pepThis effectivelly separates out all of the markers into individual multifasta .pep files that appear consistent informat with media/box1/AMPHORA_example/AMPHORA/cobmined/*.pep1
+aln_list = glob.glob("*.aln")
+if os.path.exists("combined.aln"):
+    aln_list.remove("combined.aln")
 
-!rm combined.aln
-
-import glob
-import re
-
-num_input_genome = 2
+#5.  Identify list of shared markers and extract individual aligned sequences
+#num_input_genome = 51 #--> Defined above
+bad_contig_list = []
 shared_protein_list = []
+marker_list = []
 genome_dict = {}
 num_marker_proteins = 0
-for aln in glob.glob("*.aln"):
+for aln in aln_list:
     num_marker_proteins += 1
     protein_name = aln.rstrip(".aln")
+    marker_list.append(protein_name)
     with open(aln) as infile:
         num_aln_genomes = 0
         for line in infile:
@@ -43,7 +79,10 @@ for aln in glob.glob("*.aln"):
                 if m:
                     genome = m.group()
                 else:
-                    print("No match found:\n{}".format(seq_name))
+                    print("No match found:\n{}\nExitting...".format(seq_name))
+                    print("Make sure to delete files before rerunning AMPHORA scripts...")
+                    bad_contig_list.append(seq_name)
+                    break
 
                 if genome not in genome_dict:
                     genome_dict[genome] = {}
@@ -61,26 +100,49 @@ for aln in glob.glob("*.aln"):
             #This will not properly handle the case of multiple protein
             #seqs from single genome
             shared_protein_list.append(protein_name)
-        else:
-            print("{} not presented in all {} genomes, omitting from tree...".format(protein_name,num_input_genome))
 
+
+        else:
+            print("{} genomes have {}".format(num_aln_genomes,protein_name))
+            print("{} not presented in all {} genomes, omitting from tree...".format(protein_name,num_input_genome))
 print("\nUsing {} of {} protein markers to build concatenated alignment...".format(num_marker_proteins,len(shared_protein_list)))
+
+if len(shared_protein_list) == 0:
+    for genome in genome_dict.keys():
+        print genome,len(genome_dict[genome].keys())
+        missing_markers = [marker for marker in marker_list if marker not in genome_dict[genome].keys()]
+        if len(missing_markers) > 0:
+            print ("\tMissing: {}".format(",".join(missing_markers)))
+
+#Blautia luti is missing: rpsJ,rplT,tsf,rpmA,rplF,rplM
+#Could omit this one...
+
+#6. Correct and concatenate aligned marker sequences
 combined_aln_dict = {}
 for genome in genome_dict.keys():
     combined_aln_dict[genome] = ""
     for count,shared_protein in enumerate(shared_protein_list):
         print genome,shared_protein
         seq = genome_dict[genome][shared_protein]
+        #NOTE: Should check length - Jason's note in 'amphora_merge_alignments.pl'
+        # Sometimes APMPHORA2 misses a character on the end of the last sequence
+    	# Check the alignment lengths
         corrected_seq = ""
         #Replace masked letters
         for letter in seq:
             if letter.islower():
                 corrected_seq += "-"
+            #Replace '*' with '-'
+            elif letter == "*":
+                corrected_seq += "-"
             else:
                 corrected_seq += letter
+
         combined_aln_dict[genome] += "-" * 10 + corrected_seq
 
 with open("combined.aln", "w") as outfile:
     for genome in combined_aln_dict:
         outfile.write(">{}\n".format(genome))
         outfile.write("{}\n".format(combined_aln_dict[genome]))
+
+#7. Build tree from alignment with FastTreeMP.
