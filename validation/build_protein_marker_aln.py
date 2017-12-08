d30cfb305e0a5eaabf2ecc82ed118545043c2c98
Ian Miller <ijmiller2@wisc.edu>
2017-12-08 14:51:11 -0600

    Got it working for any group of fastas now.

--- validation/build_protein_marker_aln.py
+++ validation/build_protein_marker_aln.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 import glob
 import os
@@ -5,36 +6,30 @@ import re
 import subprocess
 import pandas as pd
 from collections import Counter
+import argparse
+
+parser = argparse.ArgumentParser(description='Script to build an aligned fasta\
+    file using AMPHORA2 from a directory of fasta files.')
+parser.add_argument('-x','--extension', help='File extension to use for fasta files', default=".fasta")
+args = vars(parser.parse_args())
 
 #input = whole genome fastas
 #output = aligned fasta file with all shared protein markers
 """
 0. Standardize fasta contig names
-1. Call ORFs, translate, and identify markers with AMPHORA’s MarkerScanner.pl
+1. Call ORFs, translate, and identify markers with AMPHORA's MarkerScanner.pl
 2. Combine protein marker sequences with cat
-3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScanner.pl
-•This creates .pep files that appear consistent in format with/media/box1/AMPHORA_example/AMPHORA/cobmined/*.pep
-4. Align markers with AMPHORA’s MarkerAlignTrim.pl
+3. Reformat combined protein sequence file with another run of AMPHORA's MarkerScanner.pl
+    This creates .pep files that appear consistent in format with/media/box1/AMPHORA_example/AMPHORA/cobmined/*.pep
+4. Align markers with AMPHORA's MarkerAlignTrim.pl
 5. Identify list of shared markers and extract individual aligned sequences
 6. Correct and concatenate aligned marker sequences
 7. Build tree from alignment with FastTreeMP.
 """
-#NOTE: Had to manually add "AAA.1"  toB. luti and B. thetaiotaomicron seq names
-#to have MarkerScanner.pl parse their names properly
 
-#in: /home/ijmiller/AMPHORA2/MIX-51/test
-ref_genome_table = pd.read_csv("ncbi_ref_genomes.tab",sep="\t")
-genome_filename_dict = {}
-
-#0. Standardize fasta contig names
-for index,row in ref_genome_table.iterrows():
-    basename = row['basename']
-    genome_name = row['ref_genome']
-    genome_filename_dict[basename] = genome_name
-
-for fna in glob.glob("*.fna"):
+for fna in glob.glob("*" + args['extension']):
     basename = ".".join(fna.split(".")[:-1])
-    output_handle = basename + ".fasta"
+    output_handle = basename + "_renamed.fasta"
     input_handle = open(fna)
     with open(output_handle,"w") as outfile:
         for count,line in enumerate(input_handle):
@@ -47,7 +42,7 @@ for fna in glob.glob("*.fna"):
 
 #1. For every fasta in the working directory
 num_input_genome = 0
-for fasta in glob.glob("*.fasta"):
+for fasta in glob.glob("*_renamed.fasta"):
     #Make a new directory with the fastas base name, if it doesn't exist
     basename = ".".join(fasta.split(".")[:-1])
     if not os.path.isdir(basename):
@@ -62,12 +57,12 @@ for fasta in glob.glob("*.fasta"):
 #subprocess.call("find . -mindepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True)
 subprocess.call("find . -mindepth 2 -maxdepth 2 -name '*.pep'  | xargs -I % cat % >> combined.pep", shell=True)
 
-#3. Reformat combined protein sequence file with another run of AMPHORA’s MarkerScanner.pl
+#3. Reformat combined protein sequence file with another run of AMPHORA's MarkerScanner.pl
 subprocess.call(["mkdir","combined"])
 os.chdir("combined") #/home/ijmiller/AMPHORA2/MIX-51/fasta_test/combined
 subprocess.call(["MarkerScanner.pl","-Bacteria","../combined.pep"])
 
-#4. Align markers with AMPHORA’s MarkerAlignTrim.pl
+#4. Align markers with AMPHORA's MarkerAlignTrim.pl
 subprocess.call(["MarkerAlignTrim.pl","-OutputFormat","fasta"])
 aln_list = glob.glob("*.aln")
 if os.path.exists("combined.aln"):
@@ -94,11 +89,12 @@ for aln in aln_list:
                 skip_sequence = False
                 seq_name = line.rstrip().lstrip(">")
                 genome = seq_name.split("SENSE) ")[-1]
-                #m = re.search(r"[A-Z]{1}[a-z]+\s[a-z]+", seq_name)
-                m = re.search(r">[A-Z]+.*?-[0-9]",line.rstrip())
+                #m = re.search(r">[A-Z]+.*?-[0-9]",line.rstrip())
+                #m = re.search(r">cluster_DBSCAN_round.*?-",line.rstrip())
+                m = re.search(r">.*?-",line.rstrip())
                 if m:
-                    genome_basename = m.group()[1:-2].rstrip("_genomic")
-                    genome = genome_filename_dict[genome_basename]
+                    genome = m.group()[1:-1]
+                    #genome = genome_filename_dict[genome_basename]
                 else:
                     print("No match found:\n{}\nExitting...".format(seq_name))
                     print("Make sure to delete files before rerunning AMPHORA scripts...")
