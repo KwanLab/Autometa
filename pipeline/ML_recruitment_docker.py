6106f59b646a7ca9dccd84f17caa3b37a7b4dc35
Jason Kwan <jason.kwan@wisc.edu>
2018-01-05 08:34:17 -0600

    added docker wrapper scripts for make_taxonomy_table.py, run_autometa.py and ML_recruitment.py

---
+++ pipeline/ML_recruitment_docker.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+# Docker wrapper for ML_recruitment.py
+# Aims to be invisible to the user (use in exactly the same way as ML_recruitment.py)
+
+import argparse
+import os
+
+def run_command(command_string, stdout_path = None):
+	# Function that checks if a command ran properly. If it didn't, then print an error message then quit
+	if stdout_path:
+		f = open(stdout_path, 'w')
+		exit_code = subprocess.call(command_string, stdout=f, shell=True)
+		f.close()
+	else:
+		exit_code = subprocess.call(command_string, shell=True)
+
+	if exit_code != 0:
+		print('run_autometa_docker.py: Error, the command:')
+		print(command_string)
+		print('failed, with exit code ' + str(exit_code))
+		exit(1)
+
+parser = argparse.ArgumentParser(description="Recruit unclustered (or non-marker)\
+	sequences with Machine Learning classification using clustered sequence\
+	as training data. Features to train with include sequence coverage,\
+	composition, and homology. Confidence is calculated using jackknife\
+	cross-validation by randomly subsetting the training data n number of times.")
+parser.add_argument('-t','--contig_tab', metavar='<contig.tab>', help='Path to master contig table which includes initial clusters', required=True)
+parser.add_argument('-c','--cluster_column', metavar='<column header>', help='Name of column containing initial cluster information', \
+	default='cluster')
+parser.add_argument('-p','--processors', metavar='<int>', help='Number of processors to use', type=int, \
+	default=1)
+parser.add_argument('-r','--recursive', help='If specified, will run classification \
+	iteratively and refine traning data after each iteration.', action='store_true')
+parser.add_argument('-C','--Confidence_cutoff', metavar='<int>', help='Confidence cutoff value\
+	to use to keep ML-based predictions.', type=int, default=100)
+parser.add_argument('-u','--unclustered_name', metavar='<unclustered name>', help='Name of unclustered group \
+	in cluster column', default="unclustered")
+parser.add_argument('-n','--num_iterations', metavar='<int>', help='Number of iterations for \
+	jackknife cross-validation.', type=int, default=10)
+parser.add_argument('-m','--k_mer_matrix', metavar='<k-mer.tab>', help='Path to k-mer_matrix file.', default="k-mer_matrix")
+parser.add_argument('-o','--out_table', metavar='<output.tab>', help='Path to create output table with new column\
+	for ML-recruited sequences.',required=True)
+parser.add_argument('-k','--kingdom', metavar='<archaea|bacteria>', help='Kingdom to consider (archaea|bacteria)',\
+	choices=['bacteria','archaea'], default = 'bacteria')
+args = vars(parser.parse_args())
+
+contig_tab_path = args['contig_tab']
+cluster_column = args['cluster_column']
+processors = args['processors']
+recursive = args['recursive']
+confidence_cutoff = args['Confidence_cutoff']
+unclustered_name = args['unclustered_name']
+num_iterations = args['num_iterations']
+k_mer_matrix_path = args['k_mer_matrix']
+out_table_path = args['out_table']
+kingdom = args['kingdom']
+
+# Do check to see if the contig table and the k_mer_matrix file exists
+if not os.path.isfile(args['contig_tab']):
+	print('Error! Could not find contig table at the following path: ' + args['contig_tab'])
+	exit(1)
+
+if not os.path.isfile(args['k_mer_matrix']):
+	print('Error! Could not find k-mer matrix file at the following path: ' + args['k_mer_matrix'])
+	exit(1)
+
+# We will use the directory that will hold the output table as the output directory
+out_table_path_absolute = os.path.abspath(out_table_path)
+output_dir = '/'.join(out_table_path_absolute.split('/')[:1])
+out_table_filename = out_table_path_absolute.split('/')[-1]
+
+# We have to create the output dir if it doesn't exist
+if not os.path.isdir(output_dir):
+	os.makedirs(output_dir)
+
+#If the contig table is not already in the output dir, we need to copy it there so that the docker container can see it
+contig_tab_path_absolute = os.path.abspath(contig_tab_path)
+contig_tab_directory = '/'.join(contig_tab_path_absolute.split('/')[:-1])
+contig_tab_filename = contig_tab_path_absolute.split('/')[-1]
+
+if output_dir != contig_tab_directory:
+	# This means we need to copy the contig table to the output directory
+	run_command('cp ' + contig_tab_path_absolute + ' ' + output_dir + '/')
+
+#If k-mer matrix file is not already in the output dir, we need to copy it there so that the docker container can see it
+k_mer_matrix_path_absolute = os.path.abspath(k_mer_matrix_path)
+k_mer_matrix_directory = '/'.join(k_mer_matrix_path_absolute.split('/')[:-1])
+k_mer_matrix_filename = k_mer_matrix_path_absolute.split('/')[-1]
+
+if output_dir != k_mer_matrix_directory:
+	run_command('cp ' + k_mer_matrix_path_absolute + ' ' + output_dir + '/')
+
+# Construct ML_recruitment.py command to pass to the docker container
+ML_recruitment_command = 'ML_recruitment.py --contig_tab /output/{} --cluster_column {} --processors {} --Confidence_cutoff {} --unclustered_name {} --num_iterations {} --k_mer_matrix /output/{} --out_table /output/{} --kingdom {}'.format(\
+	contig_tab_filename, cluster_column, processors, confidence_cutoff, unclustered_name, num_iterations, k_mer_matrix_filename, out_table_filename, kingdom)
+
+if recursive:
+	ML_recruitment_command = ML_recruitment_command + ' --recursive'
+
+# Construct Docker run command
+docker_command = 'docker run --volume {}:/output:rw --detach=false --rm autometa:run'.format(output_dir)
+
+# Incorporate autometa command
+docker_command = docker_command + ' ' + ML_recruitment_command
+
+# Run docker container
+run_command(docker_command)
---
+++ pipeline/make_taxonomy_table_docker.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# Docker wrapper for make_taxonomy_table.py
+# Aims to be invisible to the user (use in exactly the same way as make_taxonomy_table.py)
+
+import argparse
+import os
+
+def run_command(command_string, stdout_path = None):
+	# Function that checks if a command ran properly. If it didn't, then print an error message then quit
+	if stdout_path:
+		f = open(stdout_path, 'w')
+		exit_code = subprocess.call(command_string, stdout=f, shell=True)
+		f.close()
+	else:
+		exit_code = subprocess.call(command_string, shell=True)
+
+	if exit_code != 0:
+		print('run_autometa_docker.py: Error, the command:')
+		print(command_string)
+		print('failed, with exit code ' + str(exit_code))
+		exit(1)
+
+#argument parser
+parser = argparse.ArgumentParser(description="Script to generate the contig taxonomy table.", epilog="Output will be directed to recursive_dbscan.py")
+parser.add_argument('-a', '--assembly', metavar='<assembly.fasta>', help='Path to metagenomic assembly fasta', required=True)
+parser.add_argument('-p', '--processors', metavar='<int>', help='Number of processors to use.', type=int, default=1)
+parser.add_argument('-db', '--db_dir', metavar='<dir>', help='Path to directory with taxdump, protein accessions and diamond (NR) protein files. If this path does not exist, will create and download files.', required=False, default=autometa_path + '/databases')
+parser.add_argument('-l', '--length_cutoff', metavar='<int>', help='Contig length cutoff to consider for binning in bp', default=10000, type = int)
+parser.add_argument('-u', '--update', required=False, action='store_true',\
+ help='Checks/Adds/Updates: nodes.dmp, names.dmp, accession2taxid, nr.dmnd files within specified directory.')
+parser.add_argument('-v', '--cov_table', metavar='<coverage.tab>', help="Path to coverage table made by calculate_read_coverage.py. If this is not specified then coverage information will be extracted from contig names (SPAdes format)", required=False)
+
+args = vars(parser.parse_args())
+
+db_dir_path = args['db_dir'].rstrip('/')
+num_processors = args['processors']
+length_cutoff = args['length_cutoff']
+fasta_path = args['assembly']
+fasta_assembly_prefix = os.path.splitext(os.path.basename(args['assembly']))[0]
+prodigal_output = fasta_assembly_prefix + "_filtered.orfs"
+prodigal_daa = prodigal_output + ".daa"
+#add_contig_path = pipeline_path
+filtered_assembly = fasta_assembly_prefix + "_filtered.fasta"
+cov_table = args['cov_table']
+update = args['update']
+
+#check if fasta in path
+if not os.path.isfile(fasta_assembly):
+	print "Could not find {}...".format(fasta_assembly)
+	logger.debug('Could not find {}...'.format(fasta_assembly))
+	exit(1)
+
+# We have to create the output dir if it doesn't exist
+if not os.path.isdir(output_dir):
+	os.makedirs(output_dir)
+
+# If db_dir is given and it doesn't exist, then it must be created
+if db_dir_path:
+	if not os.path.isdir(db_dir_path):
+		os.makedirs(db_dir_path)
+db_dir_path_absolute = os.path.abspath(db_dir_path)
+
+#If the assembly fasta is not already in the output dir, we need to copy it there so that the docker container can see it
+output_dir_absolute = os.path.abspath(output_dir)
+fasta_assembly_absolute = os.path.abspath(fasta_assembly)
+fasta_directory = '/'.join(fasta_assembly_absolute.split('/')[:-1])
+fasta_filename = fasta_assembly_absolute.split('/')[-1]
+
+if output_dir_absolute != fasta_directory:
+	# This means we need to copy the fasta assembly to the output directory
+	run_command('cp ' + fasta_assembly_absolute + ' ' + output_dir_absolute + '/')
+
+#If a coverage table is give, it must already exist.
+#If it exists, then it also should be copied to the output directory if it isn't already there
+if cov_table:
+	if not os.path.isfile(cov_table):
+		print('Error! Could not find coverage table at the following path: ' + cov_table)
+		exit(1)
+	else:
+		cov_table_absolute = os.path.abspath(cov_table)
+		cov_table_directory = '/'.join(cov_table_absolute.split('/')[:-1])
+		cov_table_filename = cov_table_absolute.split('/')[1]
+		if output_dir_absolute != cov_table_directory:
+			run_command('cp ' + cov_table_absolute + ' ' + output_dir_absolute + '/')
+
+# Construct make_taxonomy_table.py command to pass to the docker container
+make_taxonomy_table_command = 'make_taxonomy_table.py --assembly /output/{} --processors {} --db_dir /databases --length_cutoff {}'.format(\
+	fasta_filename, num_processors, length_cutoff)
+
+if cov_table:
+	make_taxonomy_table_command = make_taxonomy_table_command + ' --cov_table {}'.format(cov_table_filename)
+
+if update:
+	make_taxonomy_table_command = make_taxonomy_table_command + ' --update'
+
+# Construct Docker run command
+docker_command = 'docker run --volume {}:/output:rw --volume {}:/databases:rw --detach=false --rm autometa:run'.format(output_dir_absolute, db_dir_path_absolute)
+
+# Incorporate autometa command
+docker_command = docker_command + ' ' + make_taxonomy_table_command
+
+# Run docker container
+run_command(docker_command)
