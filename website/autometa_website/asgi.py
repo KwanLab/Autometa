6e14c6c1ce48d46d11e7027be30fd8823c436a46
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-02-02 10:26:25 -0600

    Begin django website dev

---
+++ website/autometa_website/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for autometa_website project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autometa_website.settings')
+
+application = get_asgi_application()
