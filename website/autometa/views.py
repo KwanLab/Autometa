b7cc37568cbfdcf2ec9f5913c7be00f6ee95546c
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-02-02 11:11:33 -0600

    Make startpage

--- website/autometa/views.py
+++ website/autometa/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def startpage(request):
+    return HttpResponse('<h1>Welcome to Autometa</h1>')
