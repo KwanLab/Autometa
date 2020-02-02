62f4374ced24cbb31e5b9ad4347c3c909f1fb87b
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-02-02 10:35:48 -0600

    Restart website, add first app

--- website/website/asgi.py
+++ website/website/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for autometa_website project.
+ASGI config for website project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autometa_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
 application = get_asgi_application()
