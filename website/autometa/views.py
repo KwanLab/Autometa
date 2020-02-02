88d3db5187bc23e22fd17d50edf1c5bbb00aa4ad
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-02-02 12:24:13 -0600

    Delete website; confusing naming

--- website/autometa/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-
-def startpage(request):
-    return HttpResponse('<h1>Welcome to Autometa</h1>')
