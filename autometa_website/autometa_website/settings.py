aa1dbc179eca5bcc8d2ee6b4f946c42fb49aec7a
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2020-02-04 14:05:27 -0600

    Add startpage

--- autometa_website/autometa_website/settings.py
+++ autometa_website/autometa_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'startpage.apps.StartpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
