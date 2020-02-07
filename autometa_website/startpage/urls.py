1c38cdcc4c7d89f675fcb408d227fed52820f5c9
EvanRees <erees@wisc.edu>
2020-02-07 10:31:32 -0600

    Revert "Fix links, add nav-bar, css"
    This reverts commit ecb1476c47a070ce3aeb40b7f7f41af100635947.

--- autometa_website/startpage/urls.py
+++ autometa_website/startpage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.startpage, name='startpage-home'),
-    path('about/', views.about, name='startpage-about'),
+    path('', views.startpage, name='startpage'),
+    path('about/', views.about, name='about'),
 ]
