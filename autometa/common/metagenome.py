01a432d97044be6522b969a6495be61aeffa2ba7
EvanRees <erees@wisc.edu>
2020-03-05 12:23:18 -0600

    resolved #10 Contributors added and copyright year updated to 2020.

--- autometa/common/metagenome.py
+++ autometa/common/metagenome.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Copyright 2020 Ian J. Miller, Evan R. Rees, Kyle Wolf, Siddharth Uppal,
+Shaurya Chanana, Izaak Miller, Jason C. Kwan
+
+This file is part of Autometa.
+
+Autometa is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Autometa is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Autometa. If not, see <http://www.gnu.org/licenses/>.
+
 Script containing Metagenome class for general handling of metagenome assembly
 """
 
@@ -17,7 +35,7 @@ from Bio import SeqUtils
 from autometa.common import kmers
 from autometa.common import coverage
 from autometa.common.external import prodigal
-from autometa.common.mag import Mag
+from autometa.common.mag import MAG
 from autometa.common.utilities import timeit
 from autometa.common.utilities import gunzip
 from autometa.taxonomy.majority_vote import majority_vote
