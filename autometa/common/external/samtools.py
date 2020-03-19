00b3591020d891f55e6726e7c7a5f33613db1225
Siddharth Uppal <suppal3@wisc.edu>
2020-03-18 19:26:10 -0500

    resolved #19 improved the cmd function

--- autometa/common/external/samtools.py
+++ autometa/common/external/samtools.py
@@ -26,6 +26,8 @@ Script containing wrapper functions for samtools
 import logging
 import os
 import subprocess
+import shutil
+import tempfile
 
 import pandas as pd
 import multiprocessing as mp
@@ -33,37 +35,42 @@ import multiprocessing as mp
 logger = logging.getLogger(__name__)
 
 
-def run(cmd):
-    """Run `cmd` via subprocess.
-
+def run(cmd, bam):
+    """
+    Run `cmd` via subprocess.
+    
     Parameters
     ----------
     cmd : str
         Executable input str
-
+    bam : str
+        </path/to/output/alignment.bam>
+    
     Returns
     -------
     bool
-        True if retcode == 0. i.e. Success.
-        False if retcode != 0. i.e. Failed.
-
+        True if retcode == 0. i.e. function run successfully.
+    
     Raises
-    -------
-    ExceptionName
-        Why the exception is raised.
-
+    ------
+    ChildProcessError
+        Function did not run successfully, returns retcode.
     """
+    
     logger.debug(f'cmd: {cmd}')
-    with open(os.devnull, 'w') as stdout, open(os.devnull, 'w') as stderr:
+    log_samtools_dir = os.path.dirname(bam)
+    tempdir = tempfile.mkdtemp(suffix=None, prefix='samtools', dir=log_samtools_dir)
+    samtools_stderr=os.path.join(tempdir, 'samtools_stderr')
+    samtools_stdout=os.path.join(tempdir, 'samtools_stdout')
+    with open(samtools_stdout, 'w') as stdout, open(samtools_stderr, 'w') as stderr:
         retcode = subprocess.call(cmd, stdout=stdout, stderr=stderr, shell=True)
-    if not retcode:
+        if retcode != 0:
+            raise ChildProcessError(f'Samtools not successfully run, retcode: {retcode}')
+        shutil.rmtree(tempdir, ignore_errors=True)
         return True
-    else:
-        logger.warning(f'args:{cmd} retcode:{retcode}')
-        return False
-
+    
 def sort(sam, bam, nproc=mp.cpu_count()):
-    """
+    """ 
     Views then sorts sam file by leftmost coordinates and outputs to bam.
     
     Parameters
@@ -71,13 +78,13 @@ def sort(sam, bam, nproc=mp.cpu_count()):
     sam : str
         </path/to/alignment.sam>
     bam : str
-        </path/to/output/file.bam>
+        </path/to/output/alignment.bam>
     nproc : int, optional
-        Number of processors to be used. By default uses all the processors of the system. 
-
+        Number of processors to be used. By default uses all the processors of the system 
     """
+    
     cmd = f'samtools view -@{nproc} -bS {sam} | samtools sort -@{nproc} -o {bam}'
-    run(cmd)
+    run(cmd,bam)
 
 def main(args):
     sort(args.sam, args.bam, args.nproc)
@@ -90,7 +97,8 @@ if __name__ == '__main__':
         datefmt='%m/%d/%Y %I:%M:%S %p')
     parser = argparse.ArgumentParser(description = "Takes a sam file, sorts it and returns the output to a bam file")
     parser.add_argument('--sam', help='</path/to/alignment.sam>', type=str)
-    parser.add_argument('--bam', help='</path/to/alignment.bam>', type=str)
+    parser.add_argument('--bam', help='</path/to/output/alignment.bam>', type=str)
     parser.add_argument('--nproc', help='Number of processors to use', default=mp.cpu_count(), type=int)
     args = parser.parse_args()
     main(args)
+
