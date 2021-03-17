9c494c4d47edd54f4601db377c7bef852652f726
WiscEvan <erees@wisc.edu>
2021-03-17 10:33:16 -0500

    :art: replace only first ftp in ncbi ftp filepaths

--- autometa/config/databases.py
+++ autometa/config/databases.py
@@ -403,7 +403,7 @@ class Databases:
             if self.dryrun:
                 return
 
-            rsync_fpath = ftp_fullpath.replace("ftp", "rsync")
+            rsync_fpath = ftp_fullpath.replace("ftp", "rsync", 1)
             cmd = ["rsync", "--quiet", "--archive", rsync_fpath, outfpath]
             logger.debug(f"starting {option} download")
             subprocess.run(
