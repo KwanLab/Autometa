f3ac13f62bf59ca34a2c925597a6b117505e2e00
EvanRees <erees@wisc.edu>
2021-03-25 17:17:40 -0400

    :art: Split cluster_evaluation_metrics(...) into two funcs. :art: Add get_labels(...) func to retrieve true and predicted labels from community :fire::art: Now only allow one assignments file per call to entrypoint :fire: Remove assumptions in main about directory structure :fire: Remove sorting logic :art: Add long and wide output format options (long by default)

--- autometa/validation/benchmark.py
+++ autometa/validation/benchmark.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 COPYRIGHT
-Copyright 2020 Ian J. Miller, Evan R. Rees, Kyle Wolf, Siddharth Uppal,
+Copyright 2021 Ian J. Miller, Evan R. Rees, Kyle Wolf, Siddharth Uppal,
 Shaurya Chanana, Izaak Miller, Jason C. Kwan
 
 This file is part of Autometa.
@@ -21,7 +21,7 @@ You should have received a copy of the GNU Affero General Public License
 along with Autometa. If not, see <http://www.gnu.org/licenses/>.
 COPYRIGHT
 
-Autometa binning benchmarking.
+Autometa clustering evaluation benchmarking.
 
 Script to benchmark Autometa clustering results using clustering evaluation metrics.
 # Setting seed? See: https://stackoverflow.com/a/5837352/13118765
@@ -31,17 +31,54 @@ Script to benchmark Autometa clustering results using clustering evaluation metr
 import logging
 
 import os
-from typing import Dict
+from typing import Dict, NamedTuple, Union
 import pandas as pd
+from collections import namedtuple
 
 from sklearn import metrics
 
 logger = logging.getLogger(__name__)
 
+Labels = namedtuple("Labels", ["true", "pred"])
 
-def evaluate_clustering_performance(
-    predictions: str, reference: str
-) -> Dict[str, float]:
+
+def get_categorical_labels(
+    predictions: str, reference: Union[str, pd.DataFrame]
+) -> NamedTuple:
+    pred_df = pd.read_csv(
+        predictions, sep="\t", index_col="contig", usecols=["contig", "cluster"]
+    )
+    if not isinstance(reference, pd.DataFrame) and isinstance(reference, str):
+        ref_df = pd.read_csv(reference, sep="\t", index_col="contig")
+    elif not isinstance(reference, pd.DataFrame) and not isinstance(reference, str):
+        raise ValueError(f"reference is an invalid argument type: {type(reference)}")
+    else:
+        ref_df = reference
+    # Modification here retrieves only contigs in binning dataset. This is not giving us the "complete" score.
+    ref_df = ref_df[ref_df.index.isin(pred_df.index)]
+    # Remove any contigs that were assigned as misassembled
+    ref_df = ref_df[ref_df.reference_genome != "misassembled"]
+    # Set reference_genome as category type so we can easily retrieve categorical labels
+    # NOTE: These do not need to be a one-to-one correspondence to the prediction dataframe's categorical labels
+    # (The exact label value does not matter for these metrics as we are only looking at the groupings [not classification])
+    ref_df.reference_genome = ref_df.reference_genome.astype("category")
+    # Assign "unclustered" to NA and convert 'cluster' column to categorical type
+    unclustered_idx = pred_df[pred_df.cluster == "unclustered"].index
+    pred_df.loc[unclustered_idx, "cluster"] = pd.NA
+    pred_df.cluster = pred_df.cluster.astype("category")
+    # Merge reference_assignments and predictions
+    master_df = pd.merge(pred_df, ref_df, how="left", left_index=True, right_index=True)
+    if master_df.empty:
+        raise ValueError(
+            "The provided reference community and predictions do not match!"
+        )
+    # Retrieve categorical values for each set of labels (truth and predicted)
+    labels_true = master_df.reference_genome.cat.codes.values
+    labels_pred = master_df.cluster.cat.codes.values
+    return Labels(true=labels_true, pred=labels_pred)
+
+
+def evaluate_clustering_performance(labels: NamedTuple) -> Dict[str, float]:
     """Calculate various clustering performance metrics listed below.
 
     Note
@@ -70,8 +107,8 @@ def evaluate_clustering_performance(
     predictions : str
         Path to Autometa binning results.
 
-    reference : str
-        Path to known reference community assignments to compare against Autometa binning results.
+    reference : str|pd.DataFrame
+        Path to or dataframe of known reference community assignments to compare against Autometa binning results.
 
     Returns
     -------
@@ -80,39 +117,27 @@ def evaluate_clustering_performance(
 
     Raises
     -------
-    TableFormatError
+    ValueError
+        The input arguments are not the correct type (pd.DataFrame or str)
+    ValueError
         The provided reference community and predictions do not match!
 
     """
-    pred_df = pd.read_csv(
-        predictions, sep="\t", index_col="contig", usecols=["contig", "cluster"]
-    )
-    ref_df = pd.read_csv(reference, sep="\t", index_col="contig")
-    # Modification here retrieves only contigs in binning dataset. This is not giving us the "complete" score.
-    ref_df = ref_df[ref_df.index.isin(pred_df.index)]
-    unclustered_idx = pred_df[pred_df.cluster == "unclustered"].index
-    pred_df.loc[unclustered_idx, "cluster"] = pd.NA
-    pred_df.cluster = pred_df.cluster.astype("category")
-    # Remove any contigs that were assigned as misassembled
-    ref_df = ref_df[ref_df.reference_genome != "misassembled"]
-    ref_df.reference_genome = ref_df.reference_genome.astype("category")
-    mdf = pd.merge(pred_df, ref_df, how="left", left_index=True, right_index=True)
-    labels_true = mdf.reference_genome.cat.codes.values
-    labels_pred = mdf.cluster.cat.codes.values
-    ari = metrics.adjusted_rand_score(labels_true=labels_true, labels_pred=labels_pred)
-    homogeneity_score = metrics.homogeneity_score(labels_true, labels_pred)
-    completeness_score = metrics.completeness_score(labels_true, labels_pred)
-    v_measure = metrics.v_measure_score(labels_true, labels_pred)
+    # Calculate cluster variety of cluster evaluation metrics with labels
+    ari = metrics.adjusted_rand_score(labels_true=labels.true, labels_pred=labels.pred)
+    homogeneity_score = metrics.homogeneity_score(labels.true, labels.pred)
+    completeness_score = metrics.completeness_score(labels.true, labels.pred)
+    v_measure = metrics.v_measure_score(labels.true, labels.pred)
     ami = metrics.adjusted_mutual_info_score(
-        labels_true=labels_true, labels_pred=labels_pred
+        labels_true=labels.true, labels_pred=labels.pred
     )
     gnmi = metrics.normalized_mutual_info_score(
-        labels_true=labels_true,
-        labels_pred=labels_pred,
+        labels_true=labels.true,
+        labels_pred=labels.pred,
         average_method="geometric",
     )
     fowlkes_mallows_score = metrics.fowlkes_mallows_score(
-        labels_true=labels_true, labels_pred=labels_pred
+        labels_true=labels.true, labels_pred=labels.pred
     )
     return {
         "adjusted mutual info score": ami,
@@ -134,70 +159,58 @@ def main():
         datefmt="%m/%d/%Y %I:%M:%S %p",
         level=logger.DEBUG,
     )
-    # Note: If you do not have any defaults corresponding to your parameters,
-    # you may remove the formatter class: ArgumentDefaultsHelpFormatter
-    # to reduce help text verbosity.
     parser = argparse.ArgumentParser(
-        description="Concise description of script.",
+        description="Benchmark clustering against reference assignments for the provided simulated/synthetic community.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
         "--binning",
-        help="Path to Autometa binning output",
-        required=True,
+        help="Path to Autometa binning output (May specify multiple if they all correspond to the same `--assignments` community ",
+        metavar="filepath",
         nargs="*",
     )
     parser.add_argument(
         "--assignments",
         help="Path to community reference assignments",
+        metavar="filepath",
         required=True,
-        nargs="*",
     )
     parser.add_argument(
-        "--output",
-        help="Path to write clustering evaluation metrics",
+        "--output-wide",
+        help="Path to write clustering evaluation metrics (each metric receives its own column)",
+        metavar="filepath",
         required=False,
-        default="clustering_evaluation_metrics.tsv",
+        default="clustering_benchmarks.tsv.gz",
     )
     parser.add_argument(
-        "--stacked",
-        help="Path to write stacked clustering evaluation metrics",
+        "--output-long",
+        help="Path to write clustering evaluation metrics (metrics are stacked into one 'metric' column)",
+        metavar="filepath",
         required=False,
-        default="clustering_evaluation_metrics.stack.tsv",
+        default=None,
     )
     args = parser.parse_args()
-    metrics = []
-    for binning, assignments in zip(args.binning, args.assignments):
-        # Naming of dataset currently follows simulated_communities test data directory structure
-        # Located here: https://drive.google.com/open?id=1JFjVb-pfQTv4GXqvqRuTOZTfKdT0MwhN
-        dataset = os.path.basename(os.path.realpath(os.path.dirname(binning)))
-        assert dataset in assignments, "Assignments do not match predictions!"
-        metric = evaluate_clustering_performance(
-            predictions=binning, reference=assignments
-        )
-        # dataset = "/path/to/dataset/binning.tsv.gz"
-        metric.update({"dataset": dataset})
-        metrics.append(metric)
+    all_metrics = []
+    for binning in args.binning:
+        labels = get_categorical_labels(predictions=binning, reference=args.assignments)
+        metrics = evaluate_clustering_performance(labels)
+        metrics.update({"dataset": os.path.basename(binning)})
+        all_metrics.append(metrics)
 
-    df = pd.DataFrame(metrics).set_index("dataset")
-    # e.g. index = 78.125Mbp
-    # TODO: Need to account for synthetic communities... i.e. MIX-51, MIX-51-equal, etc.
-    df.sort_index(
-        key=lambda x: x.str.replace("Mbp", "").astype(float),
-        ascending=True,
-        inplace=True,
-    )
-    df.to_csv(args.output, sep="\t", index=True, header=True)
-    logger.info(f"Wrote {df.shape[0]} datasets metrics to {args.output}")
-    # Write out stacked dataframe for visualization with `plot-cluster-evaluation-metrics.R`
-    dff = df.stack()
-    dff.index.name = ("dataset", "metric")
-    dff.name = "score"
-    dff = dff.to_frame().reset_index(level=1).rename(columns={"level_1": "metric"})
-    dff.to_csv(args.stacked, sep="\t", index=True, header=True)
-    logger.info(
-        f"Wrote {dff.index.nunique()} datasets (stacked) metrics to {args.stacked}"
-    )
+    df = pd.DataFrame(all_metrics).set_index("dataset")
+
+    df.to_csv(args.output_wide, sep="\t", index=True, header=True)
+    logger.info(f"Wrote {df.shape[0]} datasets metrics to {args.output_wide}")
+    if args.output_long:
+        # Write out stacked dataframe for visualization with `plot-cluster-evaluation-metrics.R`
+        dff = df.stack()
+        dff.index.name = ("dataset", "metric")
+        dff.name = "score"
+        dff = dff.to_frame().reset_index(level=1).rename(columns={"level_1": "metric"})
+        dff.to_csv(args.output_long, sep="\t", index=True, header=True)
+        logger.info(
+            f"Wrote {dff.index.nunique()} datasets (stacked) metrics to {args.output_long}"
+        )
 
 
 if __name__ == "__main__":
