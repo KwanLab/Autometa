b65d14e3e2ad89b72f77bd41d8247996edd8a130
ajlail98 <ajlail@wisc.edu>
2021-05-25 11:30:07 -0500

    :art: Formatted script according to template, renamed variables

--- autometa/validation/download_dataset.py
+++ autometa/validation/download_dataset.py
@@ -1,47 +1,85 @@
-# in response to issue #110
-# Intended: fetch data similarly to scikit-learn API
-# pulling data from google drive folder with simulated or synthetic communities
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+COPYRIGHT
+Copyright 2021 Ian J. Miller, Evan R. Rees, Kyle Wolf, Siddharth Uppal,
+Shaurya Chanana, Izaak Miller, Jason C. Kwan
 
-# use gdown to download data from google drive to output directory specified by the user
-# create a dictionary of the databases in the google drive
-# allow the user to call them based on size (eg '78', '156'...)
-# allow the user to specify <some/directory>
-# find that corresponding file and download it to <some/directory>
+This file is part of Autometa.
 
-# goal: autometa-download-dataset --community 78 --output <some/directory>
+Autometa is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-# prepare dependencies
+Autometa is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Autometa. If not, see <http://www.gnu.org/licenses/>.
+COPYRIGHT
+
+pulling data from google drive folder with simulated or synthetic communities
+"""
+
+
 import gdown
-import argparse
+import logging
 
-# take in commands that user input
-# including test file for now
-parser = argparse.ArgumentParser(prog='autometa-download-dataset', description='Download a simulated community file from google drive to a specified directory')
-parser.add_argument('--community', 
-                    help='specify a size of simulated community in MB', 
-                    choices=['78', '156', '312', '625', '1250', '2500', '5000', '10000', 'test'], 
-                    required=True)
-parser.add_argument('--output', 
-                    help='specify the directory to download the file', 
-                    required=True)
-args = parser.parse_args()
+logger = logging.getLogger(__name__)
 
-# provide list of database options as a dictionary with file_ids from google
-simulated = {
-    'test': '1fy3M7RnS_HGSQVKidCy-rAwXuxldyOOv',
-    '78': '15CB8rmQaHTGy7gWtZedfBJkrwr51bb2y',
-    '156': '13bkwFBIUhdWVWlAmVCimDODWF-7tRxgI',
-    '312': '1qyAu-m6NCNuVlDFFC10waOD28j15yfV-',
-    '625': '1FgMXSD50ggu0UJbZd1PM_AvLt-E7gJix',
-    '1250': '1KoxwxBAYcz8Xz9H2v17N9CHOZ-WXWS5m',
-    '2500': '1wKZytjC4zjTuhHdNUyAT6wVbuDDIwk2m',
-    '5000': '1IX6vLfBptPxhL44dLa6jePs-GRw2XJ3S',
-    '10000': '1ON2vxEWC5FHyyPqlfZ0znMgnQ1fTirqG'
-}
 
-# construct file id into a url to put into gdown
-file_id = simulated[args.community]
-url = f'https://drive.google.com/uc?id={file_id}'
+def download_dataset(dataset, outfpath):
+    # provide list of database options as a dictionary with file_ids from google
+    simulated = {
+        "test": "1fy3M7RnS_HGSQVKidCy-rAwXuxldyOOv",
+        "78": "15CB8rmQaHTGy7gWtZedfBJkrwr51bb2y",
+        "156": "13bkwFBIUhdWVWlAmVCimDODWF-7tRxgI",
+        "312": "1qyAu-m6NCNuVlDFFC10waOD28j15yfV-",
+        "625": "1FgMXSD50ggu0UJbZd1PM_AvLt-E7gJix",
+        "1250": "1KoxwxBAYcz8Xz9H2v17N9CHOZ-WXWS5m",
+        "2500": "1wKZytjC4zjTuhHdNUyAT6wVbuDDIwk2m",
+        "5000": "1IX6vLfBptPxhL44dLa6jePs-GRw2XJ3S",
+        "10000": "1ON2vxEWC5FHyyPqlfZ0znMgnQ1fTirqG",
+    }
 
-# download the specified file with gdown
-gdown.download(url, args.output)
+    # construct file id into a url to put into gdown
+    file_id = simulated[dataset]
+    url = f"https://drive.google.com/uc?id={file_id}"
+
+    # download the specified file with gdown
+    gdown.download(url, outfpath)
+
+
+def main():
+    import argparse
+    import logging as logger
+
+    logger.basicConfig(
+        format="[%(asctime)s %(levelname)s] %(name)s: %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+        level=logger.DEBUG,
+    )
+
+    parser = argparse.ArgumentParser(
+        prog="autometa-download-dataset",
+        description="Download a simulated community file from google drive to a specified directory",
+    )
+    parser.add_argument(
+        "--dataset",
+        help="specify a size of simulated community in MB",
+        choices=["78", "156", "312", "625", "1250", "2500", "5000", "10000", "test"],
+        required=True,
+    )
+    parser.add_argument(
+        "--outfpath", help="specify the directory to download the file", required=True
+    )
+    args = parser.parse_args()
+
+    download_dataset(args.dataset, args.outfpath)
+
+
+if __name__ == "__main__":
+    main()
