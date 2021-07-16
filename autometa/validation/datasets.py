12afe4b9e0875dae63b907a110385039fed41a4e
ajlail98 <ajlail@wisc.edu>
2021-07-16 12:00:49 -0500

    Using df.loc to retrieve file_id

--- autometa/validation/datasets.py
+++ autometa/validation/datasets.py
@@ -63,16 +63,15 @@ def download(
     if community_type == "synthetic" or community_type == "all":
         raise NotImplementedError
 
-    df = pd.read_csv("gdown_fileIDs.csv", dtype=str)
+    df = pd.read_csv("gdown_fileIDs.csv", dtype=str, index_col=["dataset", "file"])
     for community_size in community_sizes:
-        df_size = df[df.dataset == community_size]
         dir_path_size = os.path.join(dir_path, community_size)
         # make a new directory
         if not os.path.exists(dir_path_size):
             os.mkdir(dir_path_size)
 
         for file_name in file_names:
-            file_id = df_size[df_size.file == file_name]["file_id"].to_list()[0]
+            file_id = df.loc[(community_size, file_name), "file_id"]
             dir_path_final = os.path.join(dir_path_size, file_name)
             url = f"https://drive.google.com/uc?id={file_id}"
 
