73dae72a75d82983bf20d37338be8f861a6984ce
kaw97 <31460812+kaw97@users.noreply.github.com>
2022-09-27 14:49:20 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,11 +47,7 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel",
-<<<<<<< Updated upstream
-    "sphinx_markdown_builder"
-=======
     "sphinx_copybutton"
->>>>>>> Stashed changes
 ]
 
 todo_include_todos = True
