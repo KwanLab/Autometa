f5f9fbe2d810ac970832183034a02f0fcd723508
kaw97 <31460812+kaw97@users.noreply.github.com>
2023-03-08 12:49:42 -0600

    Revert "update config file"
    This reverts commit f30301db0420dbb7d662b2542ef813ef997cf254.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -127,18 +127,3 @@ def sphinx_apidoc(header="Autometa modules"):
 
 parse_argparse.main()
 sphinx_apidoc()
-
-# sphinx-gallery configuration
-sphinx_gallery_conf = {
-    # path to your example scripts
-    'examples_dirs': ['../sample-gallery-1', '../sample-gallery-2'],
-    # path to where to save gallery generated output
-    'gallery_dirs': ['auto_gallery-1', 'auto_gallery-2'],
-    # specify that examples should be ordered according to filename
-    'within_subsection_order': FileNameSortKey,
-    # directory where function granular galleries are stored
-    'backreferences_dir': 'gen_modules/backreferences',
-    # Modules for which function level galleries are created.  In
-    # this case sphinx_gallery and numpy in a tuple of strings.
-    'doc_module': ('SampleModule'),
-}
